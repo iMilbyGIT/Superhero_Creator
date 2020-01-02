f59b5fd2cdb19fea39a33be813bf4eb581ba696c
Adam <dev.adammilbrath@gmail.com>
2020-01-02 11:30:13 -0600

    did migration and connected superherocontroller to database

--- SuperheroCreator/Controllers/SuperHeroController.cs
+++ SuperheroCreator/Controllers/SuperHeroController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SuperheroCreator.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,12 @@ namespace SuperheroCreator.Controllers
 {
     public class SuperHeroController : Controller
     {
+        ApplicationDbContext db;
+        public SuperHeroController()
+        {
+            db = new ApplicationDbContext();
+        }
+
         // GET: SuperHero
         public ActionResult Index()
         {
